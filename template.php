c6c965e73b4dc6846dead22ac59ab72e3dd3dcf8
didik nurdyansah <didiknurdyansah@gmail.com>
2015-03-22 15:27:26 +0700

    Initial changes of Garuda UT

--- template.php
+++ template.php
@@ -4,3 +4,19 @@
  * @file
  * template.php
  */
+ 
+ function garuda_theme() {
+  $items = array();
+  // create custom user-login.tpl.php
+  $items['user_login'] = array(
+  'render element' => 'form',
+  'path' => drupal_get_path('theme', 'garuda') . '/templates',
+  'template' => 'user-login',
+  'preprocess functions' => array(
+  'garuda_preprocess_user_login'
+  ),
+ );
+return $items;
+}
+
+
